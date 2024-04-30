da6e296efe3ca98f77ff989a278601ea88df0983
callmeaf <alikhoshkar2000@gmail.com>
2024-04-30 08:56:44 +0330

    Set name for all routes.

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -4,6 +4,6 @@ use \Illuminate\Support\Facades\Route;
 
 Route::prefix(config('callmeaf-base.api.prefix_url'))->as(config('callmeaf-base.api.prefix_route_name'))->middleware(config('callmeaf-base.api.middlewares'))->group(function () {
     Route::middleware(config('callmeaf-otp.middlewares.global'))->controller(config('callmeaf-otp.controllers.global'))->group(function () {
-        Route::post('/otp/send','send')->middleware(config('callmeaf-otp.middlewares.send'));
+        Route::post('/otp/send','send')->middleware(config('callmeaf-otp.middlewares.send'))->name('otp.send');
     });
 });
